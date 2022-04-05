3032d1d67b5ae99c5cd5f6ef4b822f7bfd84f46e
guidoMoreira <48949267+guidoMoreira@users.noreply.github.com>
2022-04-05 13:13:08 -0300

    Update WiFiScan.ino

--- V1/WiFiScan.ino
+++ V1/WiFiScan.ino
@@ -2,8 +2,8 @@
 #include <WebSocketsServer.h>
 
 // Constants
-const char* ssid = "VIVOFIBRA-BC90";
-const char* password = "21E5968853";
+const char* ssid = "NOME-WIFI";
+const char* password = "SENHA";
 
 int count = 0;
 String isClicked ="No";
